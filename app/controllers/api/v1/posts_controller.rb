63de1ea49b88678a2617ab0888f83e79aaacb3f1
btmccollum <btmcco8@gmail.com>
2019-02-27 15:31:07 -0600

    updating fetch_posts logic to handle user feed

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,18 +2,51 @@ class Api::V1::PostsController < ApplicationController
     before_action :is_token_expired?, except: %i[post_test]
 
     def fetch_posts
-        posts = Faraday.new(:url => 'https://oauth.reddit.com/r/all/top')
-    
-        postlist = posts.get do |req|
-            req.headers['Authorization'] = "bearer #{current_user.authentication_token}"
-            req.headers['User-Agent'] = "Ruby:Droplet API/0.0.0 by u/unovie"
-            req.params['limit'] = 100
-            req.params['t'] = 'day'
-        end
+        subreddits = eval(request.params['0'])[:subreddits]
+        
+        # handle get request for user feed
+        if subreddits.length > 0 
+            query_string = subreddits.join(",").gsub(",", "+")
 
+            # using manual json route (old reddit api setup) instead of API endpoint, no endpoint available at this time for this type of request
+            posts = Faraday.new(:url => "https://www.reddit.com/r/#{query_string}.json")
+            postlist = posts.get do |req|
+                req.params['limit'] = 100
+            end
+            binding.pry
+        else 
+            # serving r/best by default if no feed is passed in somehow
+            posts = Faraday.new(:url => 'https://oauth.reddit.com/best')
+    
+            postlist = posts.get do |req|
+                req.headers['Authorization'] = "bearer #{current_user.authentication_token}"
+                req.headers['User-Agent'] = "Ruby:Droplet API/0.0.0 by u/unovie"
+                req.params['limit'] = 100
+                req.params['t'] = 'day'
+            end
+        end
+        
         post_hash = JSON.parse(postlist.body)
         render json: { posts: post_hash}
     end
+
+    # def fetch_posts
+    #     # binding.pry
+    #     # subreddits = eval(params['0'])[:args]
+    #     # if subreddits.length > 0 
+    #     #     posts = Faraday.new(:url => 'https://oauth.reddit.com/r/all/top')
+        # posts = Faraday.new(:url => 'https://oauth.reddit.com/best')
+    
+        # postlist = posts.get do |req|
+        #     req.headers['Authorization'] = "bearer #{current_user.authentication_token}"
+        #     req.headers['User-Agent'] = "Ruby:Droplet API/0.0.0 by u/unovie"
+        #     req.params['limit'] = 100
+        #     req.params['t'] = 'day'
+        # end
+
+    #     post_hash = JSON.parse(postlist.body)
+    #     render json: { posts: post_hash}
+    # end
 
     def post_test
         render json: { test: 'hello'}
